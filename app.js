797fe82ed9e9fc171dfa30dbca33329847a4f3a2
mmortaga <blazebubble97@gmail.com>
2019-03-23 12:31:34 +0200

    API

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res){
+  const data = [{name: "basil", age:"19"}, {name:"mohammed", age:"21"}];
+  res.send(data);
+})
+
+
+app.listen(app.get('port'), function(){
+  console.log('app running');
+})
