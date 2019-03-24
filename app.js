147d7b755b71f730e2ce0d869a7a9c111bc3518f
byshy <33957976+byshy@users.noreply.github.com>
2019-03-24 15:54:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,23 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
-  const data = {name: "basil", age:"19"};
+  const data = {
+  "120160513": {
+    "name": "Basel N. ABuhadrous",
+    "lvl": 3,
+    "major": "Comp. Sci."
+  },
+  "120160000": {
+    "name": "Mohammed Mortaga",
+    "lvl": 3,
+    "major": "Comp. Sci."
+  },
+  "120160555": {
+    "name": "Someone something",
+    "lvl": 3,
+    "major": "Comp. Sci."
+  }
+};
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.send(data);
 })
